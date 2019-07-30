4d2e372b5243fdfb7cb1aa6f490d80abbbbf7aef
J4CKOP <53121879+J4CK0P@users.noreply.github.com>
2019-07-30 18:52:46 +0700

    Create bomcall.sh

---
+++ bomcall/bomcall.sh
@@ -0,0 +1,108 @@
+#!/system/xbin/bash
+clear
+git clone https://github.com/amsitlab/smsid-java.git
+cd ./smsid-java
+chmod +x ./install
+apt install ./smsid_1.1_all.deb
+pip install requests mechanize bs4
+clear
+
+toilet -f slant --gay "SpamCall"
+sleep 1
+echo "\033[33;1m tool untuk spam"
+sleep 1
+echo "\033[36;1m★""\033[34;1mSANTUY HACKER TEAM""\033[36;1m★"
+sleep 1
+echo "\033[35;1m1.""\033[36;1mGrab"
+echo "\033[35;1m2.""\033[36;1mTokopedia"
+echo "\033[35;1m3.""\033[36;1mCostume"
+echo "\033[37;1m4.""\033[30;1mInformasi Tool Ini"
+echo "\033[31;1m0. Keluar"
+echo "\033[33;1m Pilih Angka:"
+read mrrm
+
+if [ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+echo "\033[36;1m"
+figlet "Grab"
+echo "\033[31;1mJenis Spam:""\033[37;1mSms"
+
+echo "\033[31;1mBatas:""\033[37;1mUnlimited"
+echo "\033[31;1mPemograman:""\033[37;1mPYTHON2"
+echo "\033[31;1mAuthor:""\033[37;1mJ4CKOP"
+sleep 5
+clear
+cd $HOME/T00ls/bomcall/hei
+python2 1.py
+fi
+
+if [ $mrrm = 2 ] || [ $mrrm = 2 ]
+then
+clear
+toilet -f mono9 -F gay "Toked"
+echo "\033[31;1mJenis Spam:""\033[37;1mTelepon"
+echo "\033[31;1mBatas:""\033[37;1m1 Kali"
+echo "\033[31;1mPemograman:""\033[37;1mPHP"
+echo "\033[31;1mAuthor:""\033[37;1mSGB-Team"
+sleep 5
+clear
+cd $HOME/T00ls/bomcall/hei
+php 2.php
+fi
+
+if [ $mrrm = 3 ] || [ $mrrm = 3 ]
+then
+clear
+toilet -f mono9 -F gay "Costum"
+echo "\033[31;1mJenis Spam:""\033[37;1mSms"
+echo "\033[31;1mBatas:""\033[37;1mSms Gratis"
+echo "\033[31;1mPemograman:""\033[37;1mPython2"
+echo "\033[31;1mAuthor:""\033[37;1mSHT"
+sleep 5
+clear
+cd $HOME/T00ls/bomcall/hei
+python costum.py
+fi
+
+if
+[ $mrrm = 4 ] || [ $mrrm = 4 ]
+then
+clear
+toilet -f slant --gay "LiteSpam"
+echo "\033[34;1mContact:""\033[37;1m https://bit.ly/2yarlam "
+
+
+sleep 1
+echo "\033[35;1mPemograman:""\033[36;1m Bash"
+
+echo "\033[33;1mKarya: SHT"
+sleep 1
+echo "\033[32;1mVersi: v1"
+sleep 1
+echo "\033[36;1mTeam: SANTUY HACKER TEAM"
+sleep 1
+echo "\033[31;1mSubscribe YouTube""\033[37;1m https://www.youtube.com/channel/UCn82YGg7CnKcZh7QaNkLT8Q"
+echo "\033[35;1m Spesial Thanks To: "
+echo "\033[36;1m"
+echo "SANTUY HACKER TEAM"
+sleep 1
+echo "HACKER INDONESIA"
+sleep 1
+
+echo "\033[30;1m tunggu 8 detik"
+sleep 8
+cd $HOME/T00ls/bomcall
+sh bomcall.sh
+fi
+
+
+
+if
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+then
+echo "\033[31;1m Keluar"
+sleep 1
+echo "\033[32;1m Sampai berjumpa lagi :)"
+sleep 1
+fi
